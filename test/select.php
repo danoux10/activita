fb2fde84025f42f8c3c65715f8c77707bddb06dd
danoux10 <danybarbe10@gmail.com>
2022-07-08 20:35:14 +0200

    test custom table

--- test/select.php
+++ test/select.php
@@ -1,48 +1,97 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
     <link rel="stylesheet" href="../dist/tailwind.css">
     <link rel="stylesheet" href="../css/style.css" type="text/css">
-<!--    <link rel="stylesheet" href="style.css">-->
     <link rel="stylesheet" href="styleBis.css">
-
-	<?php require '../controller/function/table.php';?>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	<?php require '../controller/function/table.php'; ?>
+	<?php require '../controller/function/select.php'; ?>
 </head>
 <body class="dark">
-<?php //include_once '../controller/navbar.php'; ?>
-
+<?php include_once '../controller/navbar.php'; ?>
 <main class="w-screen" id="Graph">
-<!--link graph-content -->
-    <section class="w-3/6 h-full flex items-center justify-center " id="graph-content">
-    <!--sublink graph-container-->
+    <!--link content graph & table data and btn container select-->
+    <section class="w-3/6 h-full flex items-center justify-center" id="graph-content">
+        <!--sublink container graph & table-->
         <div class="graph-container w-11/12 h-full ">
-        <!--anchor graphique-->
+            <!--anchor graphique content bar/pie-->
             <div id="graphique">
-                <!--subanchor bar-->
+                <!--subanchor bar graph content-->
                 <div id="bar-content">
                     <canvas id="bar"></canvas>
                 </div>
-                <!--subanchor pie-->
+                <!--subanchor pie graph content -->
                 <div id="pie-content">
                     <canvas id="pie"></canvas>
                 </div>
             </div>
-            <!--anchor tableSelect-content-->
-            <div id="tableSelect-content" class="overflow-scroll table-content">
-              <!--subanchor tableSelect in file controller functioh table.php-->
-              <?php
-				echo tableSelect();
-              ?>
-            </div>
+            <!--anchor tableSelect content -->
+            <form id="tableSelect-content" class="table-content flex justify-center flex-col">
+                <div class="table-title flex flex-row justify-around">
+                    <button type="submit" class="btn-update"><i class="fa-solid fa-pen-clip"></i> Update data</button>
+                    <p class="font-bold uppercase">De date au date</p>
+                    <button type="submit" class="btn-delete"><i class="fa-solid fa-trash-can"></i> Delete data</button>
+                </div>
+                <div class="table-head">
+                    <span class="element-title">
+                        <p>Date</p>
+                    </span>
+                    <span class="element-title">
+                        <p>Activiter</p>
+                    </span>
+                    <span class="element-title">
+                        <p>Hours Start</p>
+                    </span>
+                    <span class="element-title">
+                        <p>Hours End</p>
+                    </span>
+                    <span class="element-title">
+                        <p>Duration</p>
+                    </span>
+                    <span class="element-title">
+                        <p>Description</p>
+                    </span>
+                    <span class="element-title">
+                        <p>Select</p>
+                    </span>
+                </div>
+                <div class="table-body overflow-scroll w-full">
+                    <?php for ($i = 0; $i < 20; $i++) { ?>
+                        <div class="table-line"><!--flex justify-around border border-amber-500 -->
+                          <label class="element-body">
+                              <p>12/12/2022</p>
+                          </label>
+                          <label class="element-body">
+                              <p>coding</p>
+                          </label>
+                          <label class="element-body">
+                              <p>10:00</p>
+                          </label>
+                          <label class="element-body">
+                              <p>11:00</p>
+                          </label>
+                          <label class="element-body">
+                              <p>01:00</p>
+                          </label>
+                          <label class="element-body">
+                              <p>project activiter <?php echo $i; ?></p>
+                          </label>
+                          <label class="element-body">
+                              <input type="radio" name="tableSelect" id="act<?php echo $i; ?>">
+                          </label>
+                      </div>
+                    <?php } ?>
+                </div>
+            </form>
         </div>
-        <!--sublink btn-container graph-->
+        <!--sublink btn container graph-->
         <div class="btn-container ml-2.5">
             <ul>
                 <li id="bar-btn"><i class="fa-solid fa-chart-column"></i></li>
@@ -51,168 +100,33 @@
             </ul>
         </div>
     </section>
-    <!--link graphForm-content-->
+
+    <!--link content form and btn container for graph-->
     <section class="w-3/6 h-full flex items-center justify-between" id="graphForm-content">
-        <!--sublink fomr-container-->
+        <!--sublink form & tableToday-->
         <div class="form-container w-11/12 h-full flex flex-col items-center justify-center content-around">
-            <!--anchor form-content-->
+            <!--anchor form content-->
             <div class="h-1/2 w-full mt-5 flex items-center justify-center" id="form-content">
                 <div class="flex">
-                    <!--subanchor data-add-->
-                    <form action="" method="post" id="data-add">
-                        <fieldset class="">
-                            <legend>Add Data</legend>
-                            
-                            <span>
-                                <label>Date : </label>
-                                <input type="date">
-                            </span>
-                    
-                            <span class="select">
-                                <label>activity : </label>
-                                <!--subanchor select-add cf controller function select-->
-                                <div class="select-box">
-                                    <div class="options-container" id="options-container-add">
-                                        <?php echo selectAdd(); ?>
-                                    </div>
-                                        
-                                    <div class="selected" id="selected-add">
-                                        Select Video Category
-                                    </div>
-                                </div>
-                            </span>
-                            
-                            <div class="grid grid-cols-2 gap-4 mt-4">
-                                <span>
-                                    <label>Heure Début : </label>
-                                    <input type="time">
-                                </span>
-                    
-                                <span>
-                                    <label>Heure Fin : </label>
-                                    <input type="time">
-                                </span>
-                            </div>
-                    
-                            <span class="my-4">
-                                <label>Durée : </label>
-                                <input type="time">
-                            </span>
-                            
-                            <span class="flex items-center flex-col">
-                                <label>Description : </label>
-                                <textarea name="" value="Bonjour je suis la" cols="30" rows="2"></textarea>
-                            </span>
-                    
-                            <span class="w-full my-4">
-                                <button type="submit" class="btn-valid">Add Data</button>
-                            </span>
+                    <!--subanchor add form add data in models/form -->
+									<?php include_once "../models/form/add_data.php"; ?>
 
-                        </fieldset>
-                    </form>
-                    
-                    <!--subanchor update-data-->
-                    <form action="" method="post" id="data-update" class="">
-                        <fieldset class="">
-                            <legend>Update Data</legend>
-                            
-                                <span>
-                                    <label>Date : </label>
-                                    <input type="date">
-                                </span>
-
-                            <span class="select">
-                                <label>activity : </label>
-                                <!--subanchor select-add cf controller function select-->
-                                <div class="select-box">
-                                    <div class="options-container" id="options-container-update">
-                                        <?php echo selectUpdate();?>
-                                    </div>
-                                    
-                                    <div class="selected" id="selected-update">
-                                        Select Video Category
-                                    </div>
-                                </div>
-                            </span>
-                            
-                            <div class="grid grid-cols-2 gap-4 mt-4">
-                                <span>
-                                    <label>Heure Début : </label>
-                                    <input type="time">
-                                </span>
-                    
-                                <span>
-                                    <label>Heure Fin : </label>
-                                    <input type="time">
-                                </span>
-                            </div>
-
-                            <span class="my-4">
-                                <label>Durée : </label>
-                                <input type="time">
-                            </span>
-                    
-                    
-                            <span class="flex items-center flex-col">
-                                <label>Description : </label>
-                                <textarea name="" value="Bonjour je suis la" cols="30" rows="2"></textarea>
-                            </span>
-                    
-                            <span class="w-full my-4 flex justify-around">
-                                <button type="submit" class="btn-update">Update Data</button>
-                                <button type="submit" class="btn-delete">Delete Data</button>
-                            </span>
-
-                        </fieldset>
-                    </form>
+                    <!--subanchor add form update data in models/form -->
+									<?php include_once "../models/form/modify_data.php"; ?>
                 </div>
-                <!--subanchor select-data-->
-                <form action="" method="post" id="data-select" class="">
-                    <fieldset class="p-4">
-                        <legend>Select Data</legend>
-
-                        <span class="my-4">
-                            <label >Date Début : </label>
-                            <input type="date">
-                        </span>
-                
-                        <span>
-                            <label >Date Fin : </label>
-                            <input type="date">
-                        </span>
-
-                        <span class="select my-4">
-                                <label>activity : </label>
-                            <!--subanchor select-add cf controller function select-->
-                                <div class="select-box">
-                                    <div class="options-container" id="options-container-sel">
-                                        <div class="option option-sel">
-                                            <input type="radio" class="radio" name="" id="act">
-                                            <label for="act">all</label>
-                                        </div>
-                                        <?php echo selectSel(); ?>
-                                    </div>
-                                    
-                                    <div class="selected" id="selected-sel">
-                                        Select Video Category
-                                    </div>
-                                </div>
-                            </span>
-                        <span class="w-full mb-4">
-                            <button type="submit" class="valid-btn">Select Data</button>
-                        </span>
-                    </fieldset>
-                </form>
+                <!--subanchor add form select data in models/form -->
+							<?php include_once "../models/form/select-data.php"; ?>
             </div>
-            <!--anchor data-today-->
-            <div id="data-today" class="w-full table-content mt-16 overflow-scroll flex">
-              <!--subanchor tableToday in file controller functioh table.php -->
-              <?php
-				echo tableDay();
-			  ?>
+
+            <!--anchor tableToday content-->
+            <div id="data-today" class="w-10/12 table-content mt-16 overflow-scroll flex">
+							<?php
+								echo tableDay();
+							?>
             </div>
         </div>
-        <!--sublink btn-container formGraph-->
+
+        <!--sublink btn container graphForm-->
         <div class="btn-container mr-2.5">
             <ul>
                 <li id="add-data-btn"><i class="fa-solid fa-plus"></i></li>
@@ -224,16 +138,17 @@
     </section>
 </main>
 
-<!--<script src="script.js"></script>-->
-<script src="../scripts/select.js"></script>
 <script src="../scripts/day&night.js"></script>
 
 <script src="../scripts/pieChart.js"></script>
 <script src="../scripts/barChart.js"></script>
-<!---->
+
 <script src="../scripts/toggleMain.js"></script>
-<!---->
+
 <script src="../scripts/toggleFormData.js"></script>
 <script src="../scripts/toggleGraph.js"></script>
+
+<script src="../scripts/select.js"></script>
+<script src="../scripts/toggleBloc.js"></script>
 </body>
 </html>
